1d588e30b5073a837fac2b2358b0ec99ccc19517
afifudinn <work.afifudin@gmail.com>
2021-11-19 18:05:02 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,189 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<String> categories = [
+    'Type',
+    'Magnolia',
+    'Moss',
+    'Daffodil',
+    'Green Algae'
+  ];
+
+  Widget card({String? name, String? family, String? image, String? desc}) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+            context: context,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(12))),
+            builder: (context) {
+              return Container(
+                  padding: EdgeInsets.all(24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(name!,
+                          style: GoogleFonts.merriweather(
+                              color: Colors.teal.shade800, fontSize: 30)),
+                      Text(
+                        family!,
+                        style: GoogleFonts.inter(
+                            fontStyle: FontStyle.italic, fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
+                      Text(desc!,
+                          style:
+                              GoogleFonts.inter(fontWeight: FontWeight.w500)),
+                    ],
+                  ));
+            });
+      },
+      child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          child: Column(
+            children: [
+              Container(
+                  height: 400,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      image: DecorationImage(
+                          image: AssetImage(image!), fit: BoxFit.cover)),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      padding: EdgeInsets.all(24),
+                      width: 200,
+                      height: 96,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.teal.shade800),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(name!,
+                              style: GoogleFonts.merriweather(
+                                  color: Colors.white, fontSize: 16)),
+                          Text(
+                            family!,
+                            style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontStyle: FontStyle.italic),
+                          )
+                        ],
+                      ),
+                    ),
+                  ))
+            ],
+          )),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 48),
+            width: double.infinity,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Text('Plant Pedia',
+                        style: GoogleFonts.merriweather(
+                            fontSize: 30,
+                            color: Colors.teal.shade800,
+                            fontWeight: FontWeight.w700)),
+                    Spacer(),
+                    Icon(Icons.apps, color: Colors.teal.shade800),
+                    SizedBox(
+                      width: 24,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Container(
+                  height: 48,
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: categories.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: EdgeInsets.symmetric(horizontal: 24),
+                        decoration: BoxDecoration(
+                            color: index == 0
+                                ? Colors.teal.shade800
+                                : Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(16)),
+                        margin: EdgeInsets.only(
+                            left: index == 0 ? 24 : 0, right: 12),
+                        child: Center(
+                            child: Text(categories[index],
+                                style: GoogleFonts.inter(
+                                    fontSize: 16,
+                                    color: index == 0
+                                        ? Colors.white
+                                        : Colors.black,
+                                    fontWeight: FontWeight.w500))),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                card(
+                    name: 'Magnolia',
+                    family: 'Magnoliaceae',
+                    image: 'assets/images/magnolia.jpg',
+                    desc:
+                        'Magnolia is a large genus of about 210 flowering plant species in the subfamily Magnolioideae of the family Magnoliaceae. It is named after French botanist Pierre Magnol. Magnolia is an ancient genus. Appearing before bees evolved, the flowers are theorized to have evolved to encourage pollination by beetles'),
+                card(
+                    name: 'Moss',
+                    family: 'Bryophytes',
+                    image: 'assets/images/moss.jpg',
+                    desc:
+                        'Mosses are small, non-vascular flowerless plants in the taxonomic division Bryophyta sensu stricto. Bryophyta may also refer to the parent group bryophytes, which comprise liverworts, mosses, and hornworts. Mosses typically form dense green clumps or mats, often in damp or shady locations.'),
+                card(
+                    name: 'Daffodil',
+                    family: 'Amaryllidaceae',
+                    image: 'assets/images/daffodil.jpg',
+                    desc:
+                        'Narcissus is a genus of predominantly spring flowering perennial plants of the amaryllis family, Amaryllidaceae. Various common names including daffodil, narcissus and jonquil are used to describe all or some members of the genus.'),
+                card(
+                    name: 'Green Algae',
+                    family: 'Graphidaceae',
+                    image: 'assets/images/green-algae.jpg',
+                    desc:
+                        'The green algae are a large, informal grouping of algae consisting of the Chlorophyta and Charophyta/Streptophyta, which are now placed in separate divisions, together with the more basal Mesostigmatophyceae, Chlorokybophyceae and Spirotaenia.'),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        ));
   }
 }
